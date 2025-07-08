995abce28eabbc2790b6672cc8e84826be4aa41c
Farhod <farhod@example.com>
2025-07-08 15:25:41 +0500

    dd

--- config/settings.py
+++ config/settings.py
@@ -163,7 +163,7 @@ SECURE_HSTS_PRELOAD = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
