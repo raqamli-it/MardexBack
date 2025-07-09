f5ddfa2281fb14f5615cd9d1fa6864016cca4516
Farhod <farhod@example.com>
2025-07-09 23:28:38 +0500

    t

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sfxtedi$dh=#e!n=#nwmi35^(26o0(z556j5-7d+^%e#n3t6$z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mardex.digitallaboratory.uz']
 
