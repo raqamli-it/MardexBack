5a3dd04f6e364e63172bd6d9a944472e5651b754
Farhod <farhod@example.com>
2025-07-17 19:07:44 +0500

    222

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
+# https://docs.djangoproject.com/en/5.1/topics/i18n/.
 
 LANGUAGE_CODE = 'uz'
 
