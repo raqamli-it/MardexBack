e3e1d1c1c3aff9a6c6c8211c6938582e32ae56ae
Farhod Ganijonov <farhodganijonov@81gmail.com>
2025-09-25 16:01:51 +0500

    point map

--- worker/admin.py
+++ worker/admin.py
@@ -26,8 +26,8 @@ class ProfileAdmin(OSMGeoAdmin):  # GIS xarita widgeti uchun OSMGeoAdmin ishlata
               'role', 'gender', 'point', 'status', 'is_worker_active']
     inlines = [WorkerImageInline]
     # Xarita o'lchamlari
-    map_width = 10000  # kengligi px
-    map_height = 400  # balandligi px
+    map_width = 1100 # kengligi px
+    map_height = 600  # balandligi px
     default_lon = 69.2795532482657
     default_lat = 41.31127689979969
     default_zoom = 15
