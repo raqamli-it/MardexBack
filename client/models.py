f6e029cdab3249673c83f6f02d1399b993100cbe
Farhod Ganijonov <farhodganijonov@81gmail.com>
2025-09-25 17:20:29 +0500

    srid=4326 edded

--- client/models.py
+++ client/models.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import Point
 from django.db import models
 from job.models import CategoryJob, Job, Region, City
 from users.models import AbstractUser
@@ -48,7 +49,7 @@ class Order(models.Model):
     )
     # latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     # longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
-    point = gis_models.PointField(srid=4326, null=True, blank=True)
+    point = gis_models.PointField(srid=4326, default=Point(69.279759, 41.311081))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- users/models.py
+++ users/models.py
@@ -91,7 +91,7 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
     is_worker_active = models.BooleanField(default=True)
     is_online = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    point = gis_models.PointField(srid=4326, null=True, blank=True )
+    point = gis_models.PointField(srid=4326, default=Point(69.279759, 41.311081) )
     # latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     # longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
 
