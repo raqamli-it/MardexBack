7e61071da3384c317d4981e21cf7a1207199df96
Farhod Ganijonov <farhodganijonov@81gmail.com>
2025-09-25 17:06:14 +0500

    srid=4326 edded

--- client/models.py
+++ client/models.py
@@ -48,7 +48,7 @@ class Order(models.Model):
     )
     # latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     # longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
-    point = gis_models.PointField(geography=True, null=True, blank=True)
+    point = gis_models.PointField(srid=4326, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- users/models.py
+++ users/models.py
@@ -91,7 +91,7 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
     is_worker_active = models.BooleanField(default=True)
     is_online = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    point = gis_models.PointField(geography=True, null=True, blank=True )
+    point = gis_models.PointField(srid=4326, null=True, blank=True )
     # latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     # longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
 
