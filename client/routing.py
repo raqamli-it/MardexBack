67d7da59c887a6fa5fce0a27dc467cd8cb5d21a9
Farhod Ganijonov <farhodganijonov@81gmail.com>
2025-10-07 18:03:48 +0500

    worker location realtime update added 4

--- client/routing.py
+++ client/routing.py
@@ -1,9 +1,9 @@
 from django.urls import re_path
-from client.consumers import UserOrderConsumer, OrderActionConsumer, WorkerLocationConsumer
+from client.consumers import UserOrderConsumer, OrderActionConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/worker/$', UserOrderConsumer.as_asgi()),
     re_path(r'ws/clients/$', UserOrderConsumer.as_asgi()),
     re_path(r'ws/order-actions/$', OrderActionConsumer.as_asgi()),
-    re_path(r"ws/location/$", WorkerLocationConsumer.as_asgi()),
+    # re_path(r"ws/location/$", WorkerLocationConsumer.as_asgi()),
 ]
